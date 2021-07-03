41b1aecc85784e4be234a785ba098543a667027d
dylson <dylsonuni@gmail.com>
2021-07-03 21:53:35 +0100

    created the time using moment

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,4 @@
-
 console.log("this is just a test");
+var todayDate = moment(); /* created a var to hold present moment */
+/* slecting id of current */
+$("#currentDay").text(todayDate.format("MMM Do, YYYY"));
